7a293ddd48a708cdb7f3a2307d82d493dee2ceaf
Germanchrystan <germanchrystan@gmail.com>
2021-07-11 19:44:23 -0300

    Responsive Creations Display

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 class Item{
-	constructor(title,url){
+	constructor(title,url,backgroundPositionY){
 		this.title=title;
 		this.url=url;
+		this.backgroundPositionY=backgroundPositionY;
 	};
 }
 
 
 const itemArray=
 	[
-		new Item('DEEP EARTH','images/desktop/image-deep-earth.jpg'),
-		new Item('NIGHT ARCADE','images/desktop/image-night-arcade.jpg'),
-		new Item('SOCCER TEAM VR','images/desktop/image-soccer-team.jpg'),
-		new Item('THE GRID','images/desktop/image-grid.jpg'),
-		new Item('FROM UP ABOVE VR','images/desktop/image-from-above.jpg'),
-		new Item('POCKET BOREALIS','images/desktop/image-pocket-borealis.jpg'),
-		new Item('THE CURIOSITY','images/desktop/image-curiosity.jpg'),
-		new Item('MAKE IT FISHEYE','images/desktop/image-fisheye.jpg')
+		new Item('DEEP EARTH','images/desktop/image-deep-earth.jpg','end'),
+		new Item('NIGHT ARCADE','images/desktop/image-night-arcade.jpg','start'),
+		new Item('SOCCER TEAM VR','images/desktop/image-soccer-team.jpg','start'),
+		new Item('THE GRID','images/desktop/image-grid.jpg','center'),
+		new Item('FROM UP ABOVE VR','images/desktop/image-from-above.jpg','start'),
+		new Item('POCKET BOREALIS','images/desktop/image-pocket-borealis.jpg','start'),
+		new Item('THE CURIOSITY','images/desktop/image-curiosity.jpg','start'),
+		new Item('MAKE IT FISHEYE','images/desktop/image-fisheye.jpg','center')
 	]
 
 
@@ -30,16 +31,17 @@ window.addEventListener("load",(e)=>{
 		const newItem=document.createElement("DIV");
 
 		newItem.innerHTML=`
-					<div>
+					
 						<div class="item-image image${i}">
-							<div class="item-title"><h1>${itemArray[i].title}</h1></div>;
 						</div>
-					</div>`
+						<div class="item-title"><h1>${itemArray[i].title}</h1></div>
+					`
 
 		let newStyle =	document.createElement("STYLE");
 		newStyle.innerHTML=`.image${i}{
 								background: url('${itemArray[i].url}') no-repeat;
  								background-size: cover;
+								background-position-y: ${itemArray[i].backgroundPositionY};
  								`;
 
  		newItem.classList.add("item");						
